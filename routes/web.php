a9b70f79a404f04e004e70f60ca6e8ddd49eb519
francesco <cesaranofrancescomain@gmail.com>
2023-02-02 19:01:52 +0100

    creata nuova rotta show

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 Route::get('/', [MainController::class, 'home']);
+Route::get('/santo/{id}', [MainController::class, 'show']);
