1d70c4078d617408c3325870f268050a16a4dec2
francesco <cesaranofrancescomain@gmail.com>
2023-02-02 18:47:18 +0100

    stampata lista dei santi

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,5 +3,17 @@
 @section('content')
 
     <h1>Santi</h1> 
-    
+    <ul>
+
+        @foreach ($santi as $santo)
+            <li>
+                <h3>Nome: {{$santo -> nome }}</h3>
+                <h3>Luogo Di Nascina: {{$santo -> luogo_di_nascita}}</h3>
+                <h3>Data di Benedizione: {{$santo -> data_benedizione}}</h3>
+                <h3>Numero di Miracoli: {{$santo -> numero_miracoli}}</h3>
+            </li>
+        @endforeach
+
+    </ul>
+
 @endsection
